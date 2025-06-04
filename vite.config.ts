85a2c04adc17d04ed05d907a0fb2f09375bd1f53
liang <2501119005@qq.com>
2025-06-04 18:10:58 +0800

    添加部署信息

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/use-batch-marker-docs/',
   plugins: [vue()],
   resolve: {
     alias: {
