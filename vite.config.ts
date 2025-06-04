71846c5af5ab75dabb95c158ae1fc9e28a5bf151
liang <2501119005@qq.com>
2025-06-04 23:21:09 +0800

    调整部署文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  base: '/use-batch-marker-docs/',
+export default defineConfig(({mode}) => ({
+  base: mode === 'production' ? '/use-batch-marker-docs/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-})
+}))
