0fe532476a3b887e6581a55f8feca3e131cb526e
liang <2501119005@qq.com>
2025-06-05 16:07:10 +0800

    修改base路径

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({mode}) => ({
-  base: mode === 'production' ? '/use-batch-marker-docs/' : '/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
